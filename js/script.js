3d847fbd0f2001da41e20273cb1d77add1408b59
YanaBrewster <yana.brewster@gmail.com>
2019-05-30 19:04:18 +1200

    progress

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 
 // append
-// set interval
+// set interval *
 // first
 // last
 // remove
@@ -10,3 +10,36 @@
 // removeClass
 // css
 // settimeout
+
+function slider() {
+
+		var images = $('.slide');
+
+		for(var i=0; i < images.length; i++) {
+			images[i].( ".slide" ).addClass( "overflow: hidden;" );
+		}
+
+		$('#next').click(function() {
+      myKey+2;
+    	slider();
+    })
+
+		$('#previous').click(function() {
+		myKey-2;
+		slider();
+  })
+
+		myKey++;
+		if(myKey > images.length) {
+			myKey = 1;
+		}
+
+		images[myKey-1].style.display = "block";
+
+		setInterval(slider,10000);
+	}
+
+	var myKey = 0;
+
+
+	slider();
